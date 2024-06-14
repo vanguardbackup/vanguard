647bc25e04fd7d475d695102218e1dbda621f727
Lewis Larsen <lewis@larsens.dev>
2024-06-14 20:54:46 +0100

    feat: improved mail template

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ asset('mail_logo.png') }}" class="logo" alt="Vanguard Logo">
+</a>
+</td>
+</tr>
