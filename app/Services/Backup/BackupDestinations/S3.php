a79421b9f0a7d8aaccb6010ebf81827b28824832
Lewis Larsen <lewis@larsens.dev>
2024-06-14 12:14:21 +0100

    refactor: s3 interface improvements

--- app/Services/Backup/BackupDestinations/S3.php
+++ app/Services/Backup/BackupDestinations/S3.php
@@ -4,6 +4,7 @@ namespace App\Services\Backup\BackupDestinations;
 
 use App\Services\Backup\Contracts\BackupDestinationInterface;
 use App\Services\Backup\Traits\BackupHelpers;
+use Aws\S3\S3Client;
 use Illuminate\Support\Facades\Log;
 use League\Flysystem\AwsS3V3\AwsS3V3Adapter;
 use League\Flysystem\Filesystem;
@@ -13,11 +14,10 @@ class S3 implements BackupDestinationInterface
 {
     use BackupHelpers;
 
-    protected $client;
+    protected S3Client $client;
+    protected string $bucketName;
 
-    protected $bucketName;
-
-    public function __construct($client, $bucketName)
+    public function __construct(S3Client $client, string $bucketName)
     {
         $this->client = $client;
         $this->bucketName = $bucketName;
@@ -30,9 +30,7 @@ class S3 implements BackupDestinationInterface
         ]);
 
         return collect($result['Contents'])
-            ->filter(function ($file) use ($pattern) {
-                return str_contains($file['Key'], $pattern);
-            })
+            ->filter(fn ($file) => str_contains($file['Key'], $pattern))
             ->sortByDesc('LastModified')
             ->toArray();
     }
@@ -47,7 +45,11 @@ class S3 implements BackupDestinationInterface
 
     public function streamFiles(SFTP $sftp, string $remoteZipPath, string $fileName, string $storagePath, int $retries = 3, int $delay = 5): bool
     {
-        Log::info('Starting to stream file to S3.', ['remote_zip_path' => $remoteZipPath, 'file_name' => $fileName, 'storage_path' => $storagePath]);
+        Log::info('Starting to stream file to S3.', [
+            'remote_zip_path' => $remoteZipPath,
+            'file_name' => $fileName,
+            'storage_path' => $storagePath,
+        ]);
 
         return $this->retryCommand(function () use ($sftp, $remoteZipPath, $fileName, $storagePath) {
             $adapter = new AwsS3V3Adapter($this->client, $this->bucketName);
@@ -57,9 +59,7 @@ class S3 implements BackupDestinationInterface
             $tempFile = $this->downloadFileViaSFTP($sftp, $remoteZipPath);
             $stream = $this->openFileAsStream($tempFile);
 
-            // Use the storage path and file name to create the full path for the file in the S3 bucket
-            $fullPath = $storagePath.'/'.$fileName;
-
+            $fullPath = $storagePath . '/' . $fileName;
             $filesystem->writeStream($fullPath, $stream);
             fclose($stream);
             Log::debug('Stream written to S3.', ['file_name' => $fullPath]);
--- app/Services/Backup/Contracts/BackupDestinationInterface.php
+++ app/Services/Backup/Contracts/BackupDestinationInterface.php
@@ -2,9 +2,13 @@
 
 namespace App\Services\Backup\Contracts;
 
+use phpseclib3\Net\SFTP;
+
 interface BackupDestinationInterface
 {
     public function listFiles(string $pattern): array;
 
     public function deleteFile(string $filePath): void;
+
+    public function streamFiles(SFTP $sftp, string $remoteZipPath, string $fileName, string $storagePath, int $retries = 3, int $delay = 5): bool;
 }
