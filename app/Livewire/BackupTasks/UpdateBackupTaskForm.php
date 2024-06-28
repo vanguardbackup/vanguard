51075e7f939d99acfbdeae88de80003533bc9ebd
Lewis Larsen <lewis@larsens.dev>
2024-06-28 22:25:12 +0100

    chore: duster fixes

--- app/Livewire/BackupTasks/UpdateBackupTaskForm.php
+++ app/Livewire/BackupTasks/UpdateBackupTaskForm.php
@@ -82,7 +82,9 @@ class UpdateBackupTaskForm extends Component
     public ?string $isolatedUsername = null;
     public ?string $isolatedPassword = null;
 
-    public function updatedUseCustomCron(): void {}
+    public function updatedUseCustomCron(): void
+    {
+    }
 
     public function updatedBackupType(): void
     {
