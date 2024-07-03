e48310eda7072e8f98c6404d07baebdd14f66e89
Lewis Larsen <lewis@larsens.dev>
2024-07-03 21:35:23 +0100

    fix: missing language support

--- resources/views/tags/edit.blade.php
+++ resources/views/tags/edit.blade.php
@@ -1,4 +1,4 @@
-@section('title', 'Update Tag')
+@section('title', __('Update Tag'))
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
