8e5331d7b26679c000e75bdc3c298c88e6aa3730
Lewis Larsen <lewis@larsens.dev>
2024-07-04 15:44:20 +0100

    chore: adding caching to ci and rector

--- rector.php
+++ rector.php
@@ -1,5 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
 use Rector\Config\RectorConfig;
 use Rector\EarlyReturn\Rector\If_\ChangeAndIfToEarlyReturnRector;
 use Rector\EarlyReturn\Rector\If_\ChangeIfElseValueAssignToEarlyReturnRector;
@@ -7,18 +10,28 @@ use Rector\EarlyReturn\Rector\If_\ChangeNestedIfsToEarlyReturnRector;
 use Rector\EarlyReturn\Rector\StmtsAwareInterface\ReturnEarlyIfVariableRector;
 use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
-return RectorConfig::configure()
-    ->withPaths([
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
         __DIR__ . '/app',
-    ])
-    ->withRules([
+        __DIR__ . '/tests',
+    ]);
+
+    $rectorConfig->rules([
         DeclareStrictTypesRector::class,
         ChangeAndIfToEarlyReturnRector::class,
         ChangeIfElseValueAssignToEarlyReturnRector::class,
         ChangeNestedIfsToEarlyReturnRector::class,
         ReturnEarlyIfVariableRector::class,
-    ])
-    ->withPreparedSets(
-        deadCode: true,
-        codeQuality: true
-    );
+    ]);
+
+    $rectorConfig->sets([
+        Rector\Set\ValueObject\SetList::DEAD_CODE,
+        Rector\Set\ValueObject\SetList::CODE_QUALITY,
+    ]);
+
+    $rectorConfig->importNames();
+
+    $rectorConfig->cacheClass(FileCacheStorage::class);
+
+    $rectorConfig->cacheDirectory('./storage/rector/cache');
+};
