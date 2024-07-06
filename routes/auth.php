da44d7222e25249effca569214055186ff57c760
Lewis Larsen <lewis@larsens.dev>
2024-07-06 19:37:07 +0100

    refactor: routes

--- routes/auth.php
+++ routes/auth.php
@@ -6,17 +6,10 @@ use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
 Route::middleware('guest')->group(function () {
-    Volt::route('register', 'pages.auth.register')
-        ->name('register');
-
-    Volt::route('login', 'pages.auth.login')
-        ->name('login');
-
-    Volt::route('forgot-password', 'pages.auth.forgot-password')
-        ->name('password.request');
-
-    Volt::route('reset-password/{token}', 'pages.auth.reset-password')
-        ->name('password.reset');
+    Volt::route('register', 'pages.auth.register')->name('register');
+    Volt::route('login', 'pages.auth.login')->name('login');
+    Volt::route('forgot-password', 'pages.auth.forgot-password')->name('password.request');
+    Volt::route('reset-password/{token}', 'pages.auth.reset-password')->name('password.reset');
 
     Route::get('auth/github', [GitHubSocialiteController::class, 'redirectToProvider'])
         ->name('github.redirect');
@@ -26,13 +19,11 @@ Route::get('auth/github/callback', [GitHubSocialiteController::class, 'handlePro
     ->name('github.callback');
 
 Route::middleware('auth')->group(function () {
-    Volt::route('verify-email', 'pages.auth.verify-email')
-        ->name('verification.notice');
+    Volt::route('verify-email', 'pages.auth.verify-email')->name('verification.notice');
 
     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
         ->middleware(['signed', 'throttle:6,1'])
         ->name('verification.verify');
 
-    Volt::route('confirm-password', 'pages.auth.confirm-password')
-        ->name('password.confirm');
+    Volt::route('confirm-password', 'pages.auth.confirm-password')->name('password.confirm');
 });
