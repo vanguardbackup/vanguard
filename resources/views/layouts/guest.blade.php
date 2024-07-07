14c9f12e42a708f226995e0c258b1f2c753cfd32
Lewis Larsen <lewis@larsens.dev>
2024-07-07 13:27:48 +0100

    fix: added favicons and increased logo size on auth pages

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,11 +14,23 @@
 
         <!-- Icon -->
         @if (config('app.env') === 'local')
-            <link rel="icon" href="{{ asset('local_favicon.ico') }}" type="image/x-icon"/>
-            <link rel="shortcut icon" href="{{ asset('local_favicon.png') }}" type="image/x-icon"/>
+            <link rel="icon" href="{{ asset('local-favicon.ico') }}" type="image/x-icon"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('local-apple-touch-icon.png') }}">
+            <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('local-favicon-32x32.png') }}">
+            <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('local-favicon-16x16.png') }}">
+            <link rel="manifest" href="{{ asset('local-site.webmanifest') }}">
+            <link rel="mask-icon" href="{{ asset('local-safari-pinned-tab.svg') }}" color="#b17a32">
+            <meta name="msapplication-TileColor" content="#b17a32">
+            <meta name="theme-color" content="#ffffff">
         @else
             <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
-            <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/x-icon"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+            <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+            <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+            <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+            <link rel="mask-icon" href="{{ asset('safari-pinned-tab.svg') }}" color="#020617">
+            <meta name="msapplication-TileColor" content="#020617">
+            <meta name="theme-color" content="#020617">
         @endif
 
         <!-- Scripts -->
@@ -31,7 +43,7 @@
                     <div class="max-w-lg w-full p-10">
                         <div class="text-center">
                             <a href="/" wire:navigate>
-                                <x-application-logo class="w-32 h-32 fill-current text-primary-900 dark:text-white inline" />
+                                <x-application-logo class="w-48 h-36 fill-current text-primary-900 dark:text-white inline" />
                             </a>
                             @isset($title)
                             <h2 class="text-2xl font-extrabold text-gray-900 dark:text-gray-100 -mt-6">
