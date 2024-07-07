ebbde03ca9b2e28741ec1724e9c61d68a3685be4
lewislarsen <lewislarsen@users.noreply.github.com>
2024-07-07 13:18:23 +0000

    Dusting

--- app/Services/Backup/Destinations/S3.php
+++ app/Services/Backup/Destinations/S3.php
@@ -25,8 +25,7 @@ class S3 implements BackupDestinationInterface
     public function __construct(
         protected S3Client $client,
         protected string $bucketName
-    ) {
-    }
+    ) {}
 
     /**
      * @return array<string>
