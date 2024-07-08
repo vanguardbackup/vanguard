804ea8cdef7c8fd0147edd1a51395c496f4277f4
lewislarsen <lewislarsen@users.noreply.github.com>
2024-07-08 06:12:48 +0000

    style: automated changes to code style

--- routes/channels.php
+++ routes/channels.php
@@ -7,25 +7,30 @@ use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('backup-destinations.{id}', function ($user, $id) {
     $backupDestination = BackupDestination::find($id);
+
     return $user->id === $backupDestination->user_id;
 });
 
 Broadcast::channel('remote-servers.{id}', function ($user, $id) {
     $remoteServer = RemoteServer::find($id);
+
     return $user->id === $remoteServer->user_id;
 });
 
 Broadcast::channel('backup-tasks.{id}', function ($user, $id) {
     $backupTask = BackupTask::find($id);
+
     return $user->id === $backupTask->user_id;
 });
 
 Broadcast::channel('new-backup-task-log.{id}', function ($user, $id) {
     $backupTask = BackupTask::find($id);
+
     return $user->id === $backupTask->user_id;
 });
 
 Broadcast::channel('backup-task-log.{id}', function ($user, $id) {
     $backupTask = BackupTask::find($id);
+
     return $user->id === $backupTask->user_id;
 });
