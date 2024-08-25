179f5db7904771a5ff3a3555715da23a0f95cb8b
Lewis Larsen <lewis@larsens.dev>
2024-08-25 18:26:07 +0100

    style: Adjusted icon used

--- resources/views/livewire/backup-tasks/tables/index-item.blade.php
+++ resources/views/livewire/backup-tasks/tables/index-item.blade.php
@@ -46,7 +46,7 @@
                                      x-on:mouseenter="$dispatch('show-tags-tooltip', { id: {{ $backupTask->id }}, tags: '{{ $backupTask->listOfAttachedTagLabels() }}' })">
                                         <span
                                             class="inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-200">
-                                            @svg('hugeicons-notification-01', 'h-3 w-3 mr-1 text-gray-500 dark:text-gray-400')
+                                            @svg('hugeicons-notification-02', 'h-3 w-3 mr-1 text-gray-500 dark:text-gray-400')
                                             {{ $backupTask->notificationStreams->count() }}
                                         </span>
                                 </div>
@@ -190,7 +190,7 @@
                                  x-on:mouseenter="$dispatch('show-tags-tooltip', { id: {{ $backupTask->id }}, tags: '{{ $backupTask->listOfAttachedTagLabels() }}' })">
             <span
                 class="inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-200">
-                @svg('hugeicons-notification-01', 'h-3 w-3 mr-1 text-gray-500 dark:text-gray-400')
+                @svg('hugeicons-notification-02', 'h-3 w-3 mr-1 text-gray-500 dark:text-gray-400')
                 {{ $backupTask->notificationStreams->count() }}
             </span>
                             </div>
