c155d63f9df786074794cdebf41cd1cc86dc3fc8
Lewis Larsen <lewis@larsens.dev>
2025-04-18 01:19:50 +0100

    added mail test

--- resources/views/mail/suspension-lifted-mail.blade.php
+++ resources/views/mail/suspension-lifted-mail.blade.php
@@ -5,5 +5,7 @@ Hello {{ $user->first_name }},
 
 Your suspension has been lifted. You are now able to log in.
 
+Please be aware that any future breaking of rules will result in your account being suspended once more.
+
 {{ config('app.name') }}
 </x-mail::message>
